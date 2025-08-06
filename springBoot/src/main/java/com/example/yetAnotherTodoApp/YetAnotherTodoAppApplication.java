d57a294b0ef8c12d88cc52a367adf9b7fd6bef4e
Mateusz Laski <25green28@gmail.com>
2025-08-06 13:56:19 +0200

    backend/Initial commit

---
+++ springBoot/src/main/java/com/example/yetAnotherTodoApp/YetAnotherTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.yetAnotherTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherTodoAppApplication.class, args);
+	}
+
+}
---
+++ springBoot/src/test/java/com/example/yetAnotherTodoApp/YetAnotherTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yetAnotherTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
