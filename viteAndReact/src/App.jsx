1610f75d98b1ffc3ca50cc98fcb665e3ac840331
Mateusz Laski <25green28@gmail.com>
2025-08-09 18:36:10 +0200

    [frontend] Connect to Spring Boot backend

--- viteAndReact/src/App.jsx
+++ viteAndReact/src/App.jsx
@@ -1,51 +1,191 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import SingleTodoEntity from "./SingleTodoEntity.jsx";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      "id": 1,
-      "title": "My first todo",
-      "completed": true,
-      "modificationDate": "2025-08-08T17:02:45.280898",
-      "creationDate": "2025-08-08T17:02:45.280898"
-    },
-    {
-      "id": 2,
-      "title": "Clean my car",
-      "completed": false,
-      "modificationDate": "2025-08-08T17:02:53.905707",
-      "creationDate": "2025-08-08T17:02:53.905707"
-    },
-    {
-      "id": 3,
-      "title": "Do dishes",
-      "completed": true,
-      "modificationDate": "2025-08-08T17:03:03.0802",
-      "creationDate": "2025-08-08T17:03:03.0802"
-    },
-    {
-      "id": 4,
-      "title": "Go to sleep",
-      "completed": false,
-      "modificationDate": "2025-08-08T17:03:10.05977",
-      "creationDate": "2025-08-08T17:03:10.05977"
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = "/api/todos"
+      try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+          updateOverlay(true, "Unable to get a list of todos", '')
+          return
+        }
+
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        console.log(error);
+      }
     }
-  ])
+    fetchData().then(r => setTodos(r));
+  }, [])
+
+  async function updateCompletedState(id, newState) {
+    const url = `/api/todos/${id}`
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({completed: newState})
+    })
+
+    if (!response.ok) {
+      updateOverlay(true, "Unable to update a todo status", '')
+      return
+    }
+
+    const updatedTodo = await response.json()
+
+    setTodos(todos =>
+      todos.map(todo => todo.id === updatedTodo.id ? updatedTodo : todo)
+    )
+  }
+
+  async function updateTodoTitle(id, newTitle) {
+    const url = `/api/todos/${id}`
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({title: newTitle})
+    });
+
+    if (!response.ok) {
+      throw await response.json()
+    }
+
+    const updatedTodoTitle = await response.json();
+
+    setTodos(todos =>
+      todos.map(todo => todo.id === updatedTodoTitle.id ? updatedTodoTitle : todo)
+    )
+  }
+
+  // async function updateTodo(id) {
+  //   const url = `/api/todos/${id}`
+  //   await fetch(url)
+  //   .then(r => r.json())
+  //   .then(fetchedTodo =>
+  //     setTodos(todos =>
+  //       todos.map(todo =>
+  //         todo.id === fetchedTodo.id ? fetchedTodo : todo)
+  //     )
+  //   ).catch(e => console.log(e));
+  // }
+
+  async function handleTodoCreation(e) {
+    e.preventDefault();
+    if (todos == null)
+      return
+    const todoTitle = newTodoTitle;
+    if (todoTitle.trim().length === 0) {
+      return
+    }
+    setTodoTitle('');
+    createNewTodo(todoTitle)
+  }
+
+  async function createNewTodo(title) {
+    const url = "/api/todos";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({title: title})
+    });
+
+    if (response.ok) {
+      const newTodo = await response.json();
+
+      setTodos(todos => [newTodo, ...todos])
+    } else {
+      const responseJson = await response.json();
+      if (responseJson.error === "TodoAlreadyExistsException") {
+        updateOverlay(true, 'Wrong title', `The todo with title '${title}' already exists`);
+      } else {
+        updateOverlay(true, "Unable to create a new todo", '')
+      }
+    }
+  }
+
+  async function deleteTodo(id) {
+    const url = `/api/todos/${id}`
+    const response = await fetch(url, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      setTodos(todos =>
+      todos.filter(todo => todo.id !== id)
+     )
+    } else {
+      updateOverlay(true, `Unable to delete todo number ${id}`, '')
+    }
+  }
+
+  function updateOverlay(isOpen, title, description) {
+    setOverlay(prev => ({
+      ...prev,
+      isOpen: isOpen,
+      title: title,
+      description: description,
+    }))
+  }
+
+  const [todos, setTodos] = useState([])
+  const [newTodoTitle, setTodoTitle] = useState('')
+  const [overlay,  setOverlay] = useState({
+    isOpen: false,
+    title: 'Title',
+    description: 'Description',
+  })
+
   return (
+    <>
+      {overlay.isOpen && (<div className="overlay">
+        <div className="overlayContent">
+          <h3 className="overlayTitle">{overlay.title}</h3>
+          <p className="overlayDescription">{overlay.description}</p>
+          <button className="overlayButton" onClick={() => setOverlay(prev => ({...prev, isOpen: false}))}>Close</button>
+        </div>
+      </div>)}
       <div className="mainContainer">
-        <h1 className="applicationName">Yet another todo app</h1>
-        <div className="newTodoContainer">
-            <input className="inputField" placeholder="Enter new todo name"/>
-            <button onClick={console.log("Add new todo")} className="addTodoButton">Add</button>
-        </div>
-        <div className="todosContainer">
-          {todos.map(item =>
-            <SingleTodoEntity key={item.id} name={item.title} completed={item.completed}/>
-          )}
-        </div>
+      <h1 className="applicationName">Yet another todo app</h1>
+      <form className="newTodoContainer" onSubmit={handleTodoCreation}>
+        <input
+          name="todoTitle"
+          className="inputField"
+          placeholder="Enter new todo name"
+          value={newTodoTitle}
+          onChange={(event) => setTodoTitle(event.target.value)}
+        />
+        <button type="submit" className="addTodoButton">Add</button>
+      </form>
+      <div className="todosContainer">
+        { todos != null &&
+          (todos.map(item =>
+          <SingleTodoEntity
+            key={item.id}
+            id={item.id}
+            name={item.title}
+            completed={item.completed}
+            updateCompleted={updateCompletedState}
+            updateTitle={updateTodoTitle}
+            deleteTodo={deleteTodo}
+            showDialog={updateOverlay}
+          />
+        ))
+        }
       </div>
+    </div>
+    </>
   )
 }
 
--- viteAndReact/src/SingleTodoEntity.jsx
+++ viteAndReact/src/SingleTodoEntity.jsx
@@ -1,11 +1,48 @@
 import './SingleTodoEntity.css'
+import {useRef, useState} from "react";
 
-function TodoEntity({name, completed}) {
+function TodoEntity({id, name, completed, updateCompleted, updateTitle, deleteTodo, showDialog}) {
+  const [todoTitle, setTodoTitle] = useState(name);
+  const inputRef = useRef(null);
+
+  async function handleTitleUpdate() {
+    try {
+      if (todoTitle !== name) {
+        await updateTitle(id, todoTitle)
+      }
+    } catch (e) {
+      setTodoTitle(name)
+      if (e.error === "TodoUnexpectedSyntax") {
+        showDialog(true, 'Wrong title', "The title cannot be empty");
+        return
+      } else if (e.error === "TodoAlreadyExistsException") {
+        showDialog(true, 'Wrong title', `The todo with title '${todoTitle}' already exists`);
+        return
+      }
+      showDialog(true, 'Unable to update todo title', '');
+    }
+  }
+
     return (
         <div className="todoBox">
-          <input type="checkbox" name="completed" defaultChecked={completed} className="completedButton"/>
-          <input type="text" defaultValue={name} className="titleInputField" style={completed ? {textDecoration: 'line-through', color: 'gray'} : {}}></input>
-          <button className="deleteButton">🗑️</button>
+          <input
+            type="checkbox"
+            name="completed"
+            defaultChecked={completed}
+            onChange={() => updateCompleted(id, !completed)}
+            className="completedButton"
+          />
+          <input
+            ref={inputRef}
+            type="text"
+            value={todoTitle}
+            className="titleInputField"
+            onChange={(e) => setTodoTitle(e.target.value)}
+            onBlur={() => handleTitleUpdate()}
+            onKeyDown={(e) => e.key === "Enter" && inputRef.current.blur()}
+            style={completed ? {textDecoration: 'line-through', color: 'gray'} : {}}
+          />
+          <button className="deleteButton" onClick={() => deleteTodo(id)}>🗑️</button>
         </div>
     )
 }
